9f162275fe52a2d94e03a02ae6948fcdc122c145
unknown <patiencetiger2@gmail.com>
2026-02-03 14:40:30 +0200

    carspage

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: '../dist/admin',
+    emptyOutDir: true
+  },
   server: {
     port: 5174,
     proxy: {
